e71565f1dcc4d25f1a5b7b7a9f8a0f8ba83c135c
CristalLopezzz <156115389+CristalLopezzz@users.noreply.github.com>
2024-10-02 23:02:32 +0000

    Initial commit

---
+++ Module5Challenge/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DadJokesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] dadJokes = new string [12];
+            
+            dadJokes[0] = "What has five toes and isn't your foot? My foot.";
+            dadJokes[1] = "What's the best way to get the hospital after breaking your foot? With a tow truck.";
+            dadJokes[2] = "What do you call it when a cow grows facial hair? A moo-stache.";
+            dadJokes[3] = "There are only two things I don't eat for breakfast: Lunch and dinner.";
+            dadJokes[4] = "How do you make a tissue dance? Put a little boogie in it.";
+            dadJokes[5] = "Where do birds stay when they travel? Someplace cheep.";
+            dadJokes[6] = "I told a bad chemistry joke once. I got no reaction.";
+            dadJokes[7] = "How do mice floss their teeth? With string cheese.";
+            dadJokes[8] = "When's the best time to call your dentist? Tooth-hurty.";
+            dadJokes[9] = "Why shouldn't you tell jokes to a duck? Because they'll quack up.";
+            dadJokes[10] = "Why did the rabbit go to the salon? It was having a bad hare day.";
+            dadJokes[11] = "Why do ducks have feathers on their tales? To hide their butt-quacks.";
+
+        }
+
+            Random rnd = new Random();
+            bool moreDadJokes = true;
+
+            while (moreDadJokes)
+            {
+                Console.WriteLine("Dad joke #1: ");
+
+                //Still working on figuring out the code
+            }
+
+    } //end class
+
+} //end namespace
